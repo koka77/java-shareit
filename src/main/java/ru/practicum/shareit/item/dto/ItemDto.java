eae3f00ae14ab3ae5a0fa8edc34aa3cb8a919151
Сомов Александр Сергеевич <ASomov@EAPO.ORG>
2022-07-21 09:47:20 +0300

    добавил @NoArgsConstructor @AllArgsConstructor в айтем

--- src/main/java/ru/practicum/shareit/item/dto/ItemDto.java
+++ src/main/java/ru/practicum/shareit/item/dto/ItemDto.java
@@ -1,7 +1,9 @@
 package ru.practicum.shareit.item.dto;
 
+import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 import ru.practicum.shareit.requests.ItemRequest;
 import ru.practicum.shareit.user.model.User;
 
@@ -10,6 +12,8 @@ import javax.validation.constraints.NotNull;
 
 @Data
 @Builder
+@NoArgsConstructor
+@AllArgsConstructor
 public class ItemDto {
     private Long id;
     @NotEmpty
