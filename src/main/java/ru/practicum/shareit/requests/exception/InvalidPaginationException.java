08abb5e4efbb53593dd8040b0c13bed932275fec
Alexander Somov <debalast@ya.ru>
2022-08-28 02:43:38 +0300

    пороговые значеия jacocco пройдены. отправляю ПР

--- src/main/java/ru/practicum/shareit/requests/exception/InvalidPaginationException.java
+++
@@ -1,9 +0,0 @@
-package ru.practicum.shareit.requests.exception;
-
-import org.springframework.http.HttpStatus;
-import org.springframework.web.bind.annotation.ResponseStatus;
-
-@ResponseStatus(HttpStatus.BAD_REQUEST)
-public class InvalidPaginationException extends RuntimeException {
-
-}
